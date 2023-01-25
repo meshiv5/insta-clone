4de1f636b19163a1b352577e9da1da7eb655ce84
meshiv5 <000done9@gmail.com>
2023-01-25 14:34:56 +0530

    Updated Origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const socketioAuth = require("socketio-auth");
 const authenticate = require("./middlewares/authenticate");
 const postAuthenticate = require("./middlewares/postAuthenticate");
 const corsOptions = {
-  origin: ["https://socialgram-in.vercel.app", "http://localhost:3000"],
+  origin: "https://social-gram.onrender.com",
   credentials: true,
   optionSuccessStatus: 200,
 };
